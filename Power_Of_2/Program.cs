b82fa8904150b29090f7cdf2ac6b9a70d693c04d
Akshata72 <sableakshata880@gmail.com>
2022-03-12 21:01:12 +0530

    Added:Powers_Of_2

---
+++ Power_Of_2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PowerOf2
+{
+
+    class Powers
+    {
+       static void Main(string[]args)
+       {
+            Console.WriteLine("Enter number of Powers:");
+            int num = int.Parse(Console.ReadLine());
+            if (0 <= num << 31) 
+            {
+
+                double Power = (double)Math.Pow(2, num);
+                Console.WriteLine(Power);
+
+
+            }
+            else
+            {
+                Console.WriteLine("OverFlow....");
+            }
+            
+       }
+
+    }
+}
